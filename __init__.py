1fccb52f233da2cfdaff190234b6c8d541f20302
poh <multisosnooley@gmail.com>
2018-05-06 14:13:29 +0300

    fix codestyle

--- __init__.py
+++ __init__.py
@@ -1,49 +1,42 @@
 import os
-from cudatext import *
-from .md_find_headers import *
+import cudatext as cu
+from .md_find_headers import gen_markdown_headers
 
 #
 # http://wiki.freepascal.org/CudaText_API#How_plugin_can_fill_Code_Tree.3F
 #
 
+
 class Command:
 
     def __init__(self):
-
-        self.h_tree = app_proc(PROC_GET_CODETREE, '')
+        self.h_tree = cu.app_proc(cu.PROC_GET_CODETREE, '')
         self._test_md()
 
     def _test_md(self):
-    
+
         fn = os.path.join(os.path.dirname(__file__), 'test.md')
         with open(fn) as f:
             lines = f.read().splitlines()
-        print(markdown_find_headers(lines))
-        
+        for info in gen_markdown_headers(lines):
+            print(info)
 
     def update_tree(self):
-
-        ed.set_prop(PROP_CODETREE, False)
-
-        n = ed.get_line_count()
-
-        tree_proc(self.h_tree, TREE_ITEM_DELETE, 0)
-        tree_proc(self.h_tree, TREE_ITEM_ADD, 0, index=-1, text='Test, lines: '+str(n))
+        cu.ed.set_prop(cu.PROP_CODETREE, False)
+        n = cu.ed.get_line_count()
+        cu.tree_proc(self.h_tree, cu.TREE_ITEM_DELETE, 0)
+        cu.tree_proc(self.h_tree, cu.TREE_ITEM_ADD, 0, index=-1, text='Test, lines: '+str(n))
 
     def on_change_slow(self, ed_self):
-
         # lexer name is checked via .inf
         self.update_tree()
 
     def check_and_update(self):
-
-        if ed.get_prop(PROP_LEXER_FILE) == 'Markdown':
+        if cu.ed.get_prop(cu.PROP_LEXER_FILE) == 'Markdown':
             self.update_tree()
 
     def on_open(self, ed_self):
-    
         self.check_and_update()
-        
-    def on_tab_change(self, ed_self):
 
+    def on_tab_change(self, ed_self):
         self.check_and_update()
--- md_find_headers.py
+++ md_find_headers.py
@@ -1,47 +1,46 @@
-
-def _is_pre(s, CHAR):
-    if not s.startswith(CHAR): return
+def _is_pre(s, ch):
+    if not s.startswith(ch):
+        return
     r = 0
     for i in range(min(10, len(s))):
-        if s[i]==CHAR:
+        if s[i] == ch:
             r += 1
         else:
             break
     return r
-         
+
+
 def is_line_ticks(s):
     return _is_pre(s, '`')
 
+
 def is_line_head(s):
     return _is_pre(s, '#')
-        
 
-def markdown_find_headers(lines):
+
+def gen_markdown_headers(lines):
     '''
-    Gets list of headers, each list item is tuple
-    (line_index, level_from_1, line_text_after#)
+    Generates markdown headers in format:
+    line_number, header_level, header_text
     '''
-    
     res = []
     tick = False
     tick_r = 0
-
-    for (i, s) in enumerate(lines):
-        if not s.strip(): continue
-        
+    for i, s in enumerate(lines):
+        if not s.strip():
+            continue
         r = is_line_ticks(s)
         if r:
-            if tick and r==tick_r:
+            if tick and r == tick_r:
                 tick = False
                 tick_r = 0
             else:
                 tick = True
                 tick_r = r
             continue
-            
-        if tick: continue
+        if tick:
+            continue
         r = is_line_head(s)
         if r:
-            res += [(i, r, s[r:].strip() )]
-    
+            yield i, r - 1, s[r:].strip()
     return res
